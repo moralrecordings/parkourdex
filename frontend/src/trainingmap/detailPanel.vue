da53b111f0a61bb56fdfc751005de1f03318238a
Scott Percival <code@moral.net.au>
2018-09-25 20:08:39 +0800

    frontend: add detail panel

---
+++ frontend/src/trainingmap/detailPanel.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="grid-x">
+            <div class="cell shrink">
+                <div class="switch small">
+                    <input class="switch-input" id="testSwitch" type="checkbox" name="testSwitch">
+                    <label class="switch-paddle" for="testSwitch">
+                        <span class="show-for-sr">Add to favourites</span>
+                    </label>
+                </div> 
+            </div>
+            <div class="cell auto">
+                <label for="testSwitch">Add to favourites</label>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss">
+
+</style>
+<script>
+
+export default {
+    name: 'detailPanel',
+    data: function () {
+        return {
+
+        };
+    },
+    props: {
+        
+    },
+    methods: {
+
+    },
+    mounted: function () {
+
+    }
+}
+
+</script>
--- frontend/src/trainingmap/main.vue
+++ frontend/src/trainingmap/main.vue
@@ -1,17 +1,34 @@
 <template>
 <div class="f6inject">
-    <div class="grid-y grid-frame">
-        <l-map ref="map" v-bind:attributionControl="false" v-bind:zoom="zoom" v-bind:center="center">
+    <div class="off-canvas position-left reveal-for-medium is-open">
+        <detailPanel/>
+    </div>
+    <div class="off-canvas-content is-open-left has-position-left has-transition-push grid-y grid-frame">
+        <l-map ref="map" v-bind:attributionControl="false" v-bind:zoom="zoom" v-bind:center="center" v-bind:options="options">
             <l-tile-layer v-bind:url="basemapUrl"/>
         </l-map>
     </div>
 </div>
 </template>
-<style>
+<style lang="scss">
+
+.f6inject {
+    .off-canvas {
+        padding: 1em;
+    }
+
+    .off-canvas label {
+        line-height: inherit;
+        font-size: inherit;
+        font-weight: inherit;
+    }
+
+}
 
 </style>
 <script>
 
+import detailPanel from './detailPanel.vue';
 import { LMap, LTileLayer, LMarker, LTooltip } from 'vue2-leaflet';
 import L from 'leaflet';
 
@@ -21,6 +38,7 @@ import '../leaflet.scss';
 export default {
     name: 'mainComponent',
     components: {
+        detailPanel,
         LMap,
         LTileLayer,
         LMarker,
@@ -31,6 +49,9 @@ export default {
             zoom: 5,
             center: L.latLng(-24.966, 123.750),
             basemapUrl: null,
+            options: {
+                attributionControl: false, 
+            },
         };
     },
     props: {
