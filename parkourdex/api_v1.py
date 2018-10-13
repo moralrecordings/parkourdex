fcfaeaf1656d40ae23f9a4dae3dd8765752f6ac4
Scott Percival <code@moral.net.au>
2018-10-13 22:34:33 +0800

    parkourdex.views: init

--- parkourdex/api_v1.py
+++ parkourdex/api_v1.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
+from parkourdex.views import WhoAmIView
 from locations.api_v1 import FeatureViewSet, FeatureCategoryViewSet, LocationViewSet
 
 api_v1_router = routers.DefaultRouter()
-api_v1_router.register(r'feature', FeatureViewSet)
-api_v1_router.register(r'feature_category', FeatureCategoryViewSet)
-api_v1_router.register(r'location', LocationViewSet)
+api_v1_router.register('whoami', WhoAmIView)
+api_v1_router.register('feature', FeatureViewSet)
+api_v1_router.register('feature_category', FeatureCategoryViewSet)
+api_v1_router.register('location', LocationViewSet)
+
