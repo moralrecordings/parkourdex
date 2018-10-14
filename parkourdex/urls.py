b8e381f890ea2ede7ece5cee96cc285f482126b0
Scott Percival <code@moral.net.au>
2018-10-14 15:33:23 +0800

    parkourdex: add map url

--- parkourdex/urls.py
+++ parkourdex/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from parkourdex.api_v1 import api_v1_router
 
 urlpatterns = [
+    path( 'map/', TemplateView.as_view( template_name='trainingmap.html' ) ),
     path( 'api/v1/', include( api_v1_router.urls ) ),
     path( 'admin/', admin.site.urls ),
     path( '', include( 'django_registration.backends.activation.urls' ) ),
