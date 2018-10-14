709eef309003b73c052b27f25d6f51e9cf9b2f33
Scott Percival <code@moral.net.au>
2018-10-14 11:05:56 +0800

    frontend.about: add login box

--- frontend/src/trainingmap/aboutPanel.vue
+++ frontend/src/trainingmap/aboutPanel.vue
@@ -1,6 +1,31 @@
 <template>
     <div class="grid-container full">
-        <center><img v-bind:src="logoUrl" title="parkourdex"/></center>
+        <div class="text-center" style="margin-bottom: 1em;">
+            <img v-bind:src="logoUrl" title="parkourdex"/>
+        </div>
+        <div class="grid-x">
+            <input type="text" placeholder="Username" v-model="username"/>
+        </div>
+        <div class="grid-x">
+            <input type="password" placeholder="Password" v-model="password"/>
+        </div>
+        <div class="grid-x">
+            <button class="button expanded">Sign in</button>
+        </div>
+        <div class="text-center">
+            <a target="_blank" v-bind:href="`${parkourdexUrl}/password_reset/`" >Forgot your password?</a>
+            <hr style="width: 100%"/>
+        </div>
+        <div class="grid-x">
+            <a target="_blank" v-bind:href="`${parkourdexUrl}/register/`" class="button expanded success">Register</a>
+        </div>
+
+        <p class="text-center">
+            <small>
+                Made by <a href="https://twitter.com/moralrecordings">@moralrecordings</a> for <a href="https://perthparkour.com">Perth Parkour</a><br/> 
+                <a href="https://bitbucket.org/moralrecordings/parkourdex">Source code</a> - <a href="#">Terms of service</a> - <a href="mailto:abuse@parkourdex.org">Report abuse</a><br/>
+            </small>
+        </p>
         <button class="close-button" type="button" v-on:click="$emit('showPanel', 'about', null)"><span aria-hidden="true">×</span></button>
     </div>
 </template>
@@ -17,6 +42,8 @@ export default {
     name: 'aboutPanel',
     data: function () {
         return {
+            username: '',
+            password: '',
             logoUrl: `${this.parkourdexUrl}${logoUrl}`
         };
     },
